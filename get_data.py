113903a9116c4a6301f58c3e029335565f256773
Xiaodi Lu <kristen.lu@mail.utoronto.ca>
2017-01-31 19:54:46 -0500

    fixed gray photos displaying green problem

--- get_data.py
+++ get_data.py
@@ -80,11 +80,54 @@ for a in act:
                     im = imread("uncropped/" + filename);
                     face = im[int(y1):int(y2), int(x1):int(x2)]
                     image = imresize(face, [32, 32])
-                    imsave("cropped/" + filename, rgb2gray(image))
+                    imsave("cropped/" + filename, rgb2gray(image), cmap = cm.gray)
                 except:
                     continue
             print filename
             i += 1
     
 #def part2():
-#part1()    
+trainning_set = 100
+test_set = 10
+validation_set = 10
+
+folder = os.listdir("cropped/")
+
+for image in filename:
+    f_name = image.split(".")[0]
+    n_list = [v for v in f_name if v.isalpha()]
+    name = f_name[0:len(n_list)]
+    
+    if not os.path.exists(name + "/"):
+        os.mkdir(name + "/")
+    
+    try:
+        val = int(f_name.split(name)[1])
+    
+    except:
+        continue
+    
+    if val < trainning_set:
+        if not os.path.exists(n + "/trainning_set/"):
+            os.mkdir(name + "/trainning_set/")
+        shutil.copy("cropped/" + image, name + "/trainning_set/" + image)
+        
+    elif trainning_set <= val < trainning_set + test_set:
+        if not os.path.exists(name + "/test_set"):
+            os.mkdir(name + "/test_set/")
+        
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
